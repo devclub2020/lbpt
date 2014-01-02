686f2549af9847f615db1bbebea2d42ec5e1e5fc
vProjects <github@vyrazu.com>
2014-01-02 15:24:36 +0530

    Dipanjan
    adding css and v-admin section

---
+++ v-admin/v-templates/sidebar.php
@@ -0,0 +1,79 @@
+<!--sidebar for navigation-->
+<div class="span3" id="sidebar_container">
+    <div class="accordion" id="accordion2">
+        <!--1st accordion menu starts here-->
+        <div class="accordion-group">
+            <div class="accordion-heading">
+                <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseOne">
+                <span class="icon-list-alt"></span>&nbsp;&nbsp;Dashboard
+                </a>
+            </div>
+            <div id="collapseOne" class="accordion-body collapse">
+                <div class="accordion-inner">
+                    <ul class="nav nav-pills nav-stacked">
+                        <!--<li><a href="listMembers.php"><span class="icon-hand-right"></span>&nbsp;&nbsp;List Members</a></li>
+                        <li><a href="mlmDetails.php"><span class="icon-hand-right"></span>&nbsp;&nbsp;MLM Details</a></li>
+                    	<li><a href="upgradeMembershipValidiation.php"><span class="icon-hand-right"></span>&nbsp;&nbsp; Membership Validiation</a></li>
+                        <li><a href="addFreeMember.php"><span class="icon-hand-right"></span>&nbsp;&nbsp; Add Free Member</a></li>-->
+                    </ul>
+                </div>
+            </div>
+        </div><!--1st accordion menu starts here-->
+        
+        
+        <!--2th accordion menu starts here-->
+        <div class="accordion-group">
+            <div class="accordion-heading">
+                <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseTwo">
+                <span class="icon-list-alt"></span>&nbsp;&nbsp;Manage Content
+                </a>
+            </div>
+            <div id="collapseTwo" class="accordion-body collapse">
+                <div class="accordion-inner">
+                    <ul class="nav nav-pills nav-stacked">
+                        <li><a href="mypage.php"><span class="icon-hand-right"></span>&nbsp;&nbsp;Create New Page</a></li>
+                        <!--<li><a href="listmypage.php"><span class="icon-hand-right"></span>&nbsp;&nbsp;List My Pages</a></li>
+                        <li><a href="addFooterLinks.php"><span class="icon-hand-right"></span>&nbsp;&nbsp;Add Footer Links</a></li>
+                        <li><a href="listFooterLinks.php"><span class="icon-hand-right"></span>&nbsp;&nbsp;List Footer Links</a></li>
+                        <li><a href="editFooterBottomText.php"><span class="icon-hand-right"></span>&nbsp;&nbsp;Edit Footer Text</a></li>
+                        <li><a href="editSliderContent.php"><span class="icon-hand-right"></span>&nbsp;&nbsp;Edit Slider Content</a></li>-->
+
+                    </ul>
+                </div>
+            </div>
+        </div><!--2th accordion menu starts here-->
+        
+        <!--3th accordion menu starts here-->
+         <div class="accordion-group">
+            <div class="accordion-heading">
+                <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseThree">
+                <span class="icon-list-alt"></span>&nbsp;&nbsp;Admin Control
+                </a>
+            </div>
+            <div id="collapseThree" class="accordion-body collapse">
+                <div class="accordion-inner">
+                    <ul class="nav nav-pills nav-stacked">
+                        <li><a href="changePwd.php"><span class="icon-hand-right"></span>&nbsp;&nbsp;Change Password</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div><!--3th accordion menu starts here-->
+        
+        <!--4th accordion menu starts here-->
+         <div class="accordion-group">
+            <div class="accordion-heading">
+                <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseFour">
+                <span class="icon-list-alt"></span>&nbsp;&nbsp;Manage Blog
+                </a>
+            </div>
+            <div id="collapseFour" class="accordion-body collapse">
+                <div class="accordion-inner">
+                    <ul class="nav nav-pills nav-stacked">
+                        <!--<li><a href="changePwd.php"><span class="icon-hand-right"></span>&nbsp;&nbsp;Change Password</a></li>-->
+                    </ul>
+                </div>
+            </div>
+        </div><!--4th accordion menu starts here-->
+        
+    </div>
+</div><!--sidebar ends here-->
